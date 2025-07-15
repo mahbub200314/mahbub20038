92d7d72e089db9cf9edaa4a64bff8d7867a03686
Mahbub <mrmahbuburrahman123@gmail.com>
2025-07-16 00:32:56 +0600

    my story add in suspense

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,17 @@ import Home from './components/Home/Home'
 import Layout from './Layout'
 // import About from './components/About/About'
 // import Project from './components/Project/Project'
-import MyStory from './components/My Blogs/MyStory'
+//import MyStory from './components/My Blogs/MyStory'
 import CustomCursor from './components/cursor effect/CustomCursor'
 import Resume from './components/Resume'
 import { lazy, Suspense } from 'react'
 import Project from './components/Project/Project'
 import About from './components/About/About'
+import MyStory from './components/My Blogs/MyStory'
 
 const about = lazy(()=> import('./components/About/About'))
 const project = lazy(() => import('./components/Project/Project'))
-
+const mystory = lazy(()=> import('./components/My Blogs/MyStory'))
 function App() {
 
 
@@ -26,16 +27,22 @@ function App() {
         <Route path='/' element={<Layout />} >
 
           <Route index element={<Home />} />
+
           <Route path='about' element={
             <Suspense fallback={<div>Loading...</div>}>
                 <About/>
             </Suspense>} />
 
           <Route path='project' element={
-            <Suspense fallback={<div>loading...</div>}>
+            <Suspense fallback={<div>Loading...</div>}>
               <Project />
             </Suspense>} />
-          <Route path='mystory' element={<MyStory />} />
+
+          <Route path='mystory' element={
+            <Suspense fallback={<div>Loading..</div>}>
+              <MyStory></MyStory>
+            </Suspense>
+          } />
           <Route path='resume' element={<Resume />} />
 
         </Route>
